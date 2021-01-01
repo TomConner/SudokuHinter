319c0fea1160a0a23541b86f44efcea706a86e2a
TomConner <tomconner46@gmail.com>
2021-01-01 13:08:37 -0500

    test for test_iters_all_rows_cols_squares

--- src/hinter.py
+++ src/hinter.py
@@ -1,4 +1,25 @@
-# from pprint import pprint
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def list_of_empty_lists():
+    return [[], [], [], [], [], [], [], [], []]
+
+
+def empty_3_by_3_matrix():
+    return [
+        [
+            [], [], [],
+        ],
+        [
+            [], [], [],
+        ],
+        [
+            [], [], [],
+        ],
+    ]
 
 
 def print_board(board):
@@ -48,12 +69,13 @@ def square_iter(row_num, col_num):
 
 def possibilities(board, row_num, col_num):
     result = ''
-    for num in '123456789':
-        row = row_string(board, row_num)
-        col = col_string(board, col_num)
-        square = square_string(board, row_num, col_num)
-        if num not in row and num not in col and num not in square:
-            result += num
+    if cell(board, row_num, col_num) == '.':
+        for num in '123456789':
+            row = row_string(board, row_num)
+            col = col_string(board, col_num)
+            square = square_string(board, row_num, col_num)
+            if num not in row and num not in col and num not in square:
+                result += num
     return result
 
 
@@ -87,10 +109,87 @@ def find_hint_one_possibility(board):
         return None
 
 
-def solve(board):
+def iters_all_rows_cols_squares():
+    for row_num in range(9):
+        logging.debug('iters_all_rows_cols_squares row {}'.format(row_num))
+        yield row_iter(row_num)
+    for col_num in range(9):
+        logging.debug('iters_all_rows_cols_squares col {}'.format(col_num))
+        yield col_iter(col_num)
+    square_coords = [
+        (0, 0), (3, 0), (6, 0),
+        (0, 3), (3, 3), (6, 3),
+        (0, 6), (3, 6), (6, 6)
+    ]
+    for row_num, col_num in square_coords:
+        logging.debug(
+            'iters_all_rows_cols_squares square {} {}'.format(row_num, col_num)
+        )
+        yield square_iter(row_num, col_num)
+
+
+def get_poss_lists(board, iter):
+    poss_lists = []
+    poss_values = set()
+    for row_num, col_num in iter:
+
+        # get the list of possible values for this cell if empty
+        poss_list = possibilities(board, row_num, col_num)
+
+        # build a set of unique values that could go in cells in this list
+        for value in poss_list:
+            poss_values.add(value)
+
+        # append a tuple of coordinates and list of possibilities
+        poss_lists.append((row_num, col_num, poss_list))
+
+    return poss_lists, poss_values
+
+
+def find_hint(board):
+    # separately consider each row, column, and square
+    for iter in iters_all_rows_cols_squares():
+        # process one row, column, or square
+        result = None
+        poss_lists, poss_values = get_poss_lists(board, iter)
+
+        # find a value that can go in exactly one cell
+        for value in poss_values:
+            value_possible_position_count = 0
+            for row_num, col_num, poss_list in poss_lists:
+                if value in poss_list:
+                    value_possible_position_count += 1
+            if value_possible_position_count == 1:
+                # we have found a value that can only fit in one row,
+                # column, or square
+                result = board_set(board, row_num, col_num, value)
+                # mission accomplished, just return the one hint and be done
+                break  # out of value loop
+            if value_possible_position_count > 1:
+                # stop considering this value; we don't know where it's going
+                break  # out of value loop
+
+        if result is not None:
+            break  # out of iter loop
+
+    return result
+
+
+def solve1(board):
     result = [board]
     while True:
         next_board = find_hint_one_possibility(board)
+        if next_board is None or next_board == board:
+            break
+        board = next_board
+        result.append(board)
+    return result
+
+
+def solve(board):
+    result = [board]
+    while True:
+        next_board = find_hint(board)
         if next_board is None or next_board == board:
             break
         board = next_board
--- test/test_hinter.py
+++ test/test_hinter.py
@@ -107,3 +107,35 @@ def test_square_iter_88():
         (7, 6), (7, 7), (7, 8),
         (8, 6), (8, 7), (8, 8),
     ]
+
+
+def test_iters_all_rows_cols_squares():
+    all_lists = []
+
+    for iter in hinter.iters_all_rows_cols_squares():
+        coord_list = [(row_num, col_num) for row_num, col_num in iter]
+        all_lists.append(coord_list)
+
+    # row iters are 0-8
+    # list 2 is row 2
+    assert all_lists[2] == [
+        (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7), (2, 8)
+    ]
+
+    # col iters are 9-17
+    # list 11 is col 2
+    assert all_lists[11] == [
+        (0, 2), (1, 2), (2, 2), (3, 2), (4, 2), (5, 2), (6, 2), (7, 2), (8, 2)
+    ]
+
+    # square iters are 18-26
+    # list 18 is upper left square (0,0 through 2,2)
+    assert all_lists[18] == [
+        (0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)
+    ]
+
+    # square iters are 18-26
+    # list 22 is middle square (3,3 through 5,5)
+    assert all_lists[22] == [
+        (3, 3), (3, 4), (3, 5), (4, 3), (4, 4), (4, 5), (5, 3), (5, 4), (5, 5)
+    ]
