80588579d019dee37c5b81ffc301225a4d2756ec
TomConner <tconner@broadinstitute.org>
2022-06-03 19:51:55 -0400

    fix vulnerability

--- src/extra.py
+++ src/extra.py
@@ -1,5 +1,5 @@
 from lxml import etree
 
-parser = etree.XMLParser(resolve_entities=True)  # Noncompliant
+parser = etree.XMLParser(resolve_entities=False, no_network=True)
 tree1 = etree.parse('ressources/xxe.xml', parser)
 root1 = tree1.getroot()
